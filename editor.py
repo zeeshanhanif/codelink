3bc4a6c135e52b528ae94fc58ee95d22c714eb09
zeeshanhanif <zeeshanhanif@gmail.com>
2017-12-04 14:13:25 +0500

    Updates

--- editor.py
+++ editor.py
@@ -5,40 +5,27 @@ import javascript
 
 from browser import document as doc, window, alert
 
-has_ace = True
-editor = window.ace.edit("codeEditor")
-
-if hasattr(window, 'localStorage'):
-    from browser.local_storage import storage
-else:
-    storage = None
+editor = window.ace.edit("ace0")  
+currentState = window.getCodeState
+consoleId = "console0"
 
 if 'set_debug' in doc:
     __BRYTHON__.debug = int(doc['set_debug'].checked)
 
 def reset_src():
-    if storage is not None and "py_src" in storage:
-        editor.setValue(storage["py_src"])
-    else:
-        editor.setValue('for i in range(10):\n\tprint(i)')
+    editor.setValue(currentState())
     editor.scrollToRow(0)
     editor.gotoLine(0)
-
-def reset_src_area():
-    if storage and "py_src" in storage:
-        editor.value = storage["py_src"]
-    else:
-        editor.value = 'for i in range(10):\n\tprint(i)'
 
 class cOutput:
 
     def write(self, data):
-        doc["console"].value += str(data)
+        doc[consoleId].value += str(data)
 
     def flush(self):
         pass
 
-if "console" in doc:
+if consoleId in doc:
     sys.stdout = cOutput()
     sys.stderr = cOutput()
 
@@ -48,8 +35,8 @@ def to_str(xx):
 output = ''
 
 def show_console(ev):
-    doc["console"].value = output
-    doc["console"].cols = 60
+    doc[consoleId].value = output
+    doc[consoleId].cols = 60
 
 # load a Python script
 def load_script(evt):
@@ -57,12 +44,14 @@ def load_script(evt):
     editor.setValue(open(_name).read())
 
 # run a script, in global namespace if in_globals is True
-def run(*args):
+def run(ev):
+    global editor
     global output
-    doc["console"].value = ''
+    global consoleId
+    editor = window.ace.edit(ev.target.getAttribute("aceid"))  
+    consoleId = ev.target.getAttribute("consoleid")
+    doc[consoleId].value = ''
     src = editor.getValue()
-    if storage is not None:
-       storage["py_src"] = src
 
     t0 = time.perf_counter()
     try:
@@ -73,23 +62,15 @@ def run(*args):
         # traceback.print_exc(file=sys.stderr)
         print(exc)
         state = 0
-    output = doc["console"].value
-
+    output = doc[consoleId].value
     # print('<completed in %6.2f ms>' % ((time.perf_counter() - t0) * 1000.0))
     return state
 
 def returnValue():
-    src = editor.getValue()
-    if storage is not None:
-        storage["py_src"] = src
     return editor.getValue()
 
 def show_js(ev):
     src = editor.getValue()
-    doc["console"].value = javascript.py2js(src, '__main__')
+    doc[consoleId].value = javascript.py2js(src, '__main__')
 
-
-if has_ace:
-    reset_src()
-else:
-    reset_src_area()
+reset_src()
